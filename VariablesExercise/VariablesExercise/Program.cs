4e8e0198a18fdd803b7735f3ed87c88c3c611bd2
Vicky <vickyn5104@gmail.com>
2025-09-25 08:47:44 -0400

    added hero and villain string variables

--- VariablesExercise/VariablesExercise/Program.cs
+++ VariablesExercise/VariablesExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Variables
+            string heroFirstName = "Vicky";
+            string heroLastName = "Ni";
+            string heroFullName;
+
+            string villainTitle = "Rose Garden Witch";
+            string minionTitle = "Familiar";
+            string villainName;
+            string minion1Name;
+            string minion2Name;
+
+            //Begin Code Instructions
+            heroFullName = heroFirstName + " " + heroLastName;
+
+            Console.WriteLine("Behold the legendary hero of programming, whose " +
+                "name is: " + heroFullName);
+
+            villainName = "Gertrud";
+            minion1Name = "Anthony";
+            minion2Name = "Adelbert";
+
+            Console.WriteLine(heroFullName + "'s enemy is: " + villainName + " the " + villainTitle);
+            Console.WriteLine(villainName + "'s minions are: " + minion1Name + " the " + minionTitle +
+                " and " + minion2Name + " the " + minionTitle);
         }
     }
 }
